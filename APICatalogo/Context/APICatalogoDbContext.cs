40cca7db412cd21f79946e36dbb56fbcf6dcf415
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2023-11-19 20:33:26 -0300

    Criando Entidades, Instalando Pacotes e Criando Contexto de EF

---
+++ APICatalogo/Context/APICatalogoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class APICatalogoDbContext : DbContext
+{
+    public APICatalogoDbContext(DbContextOptions<APICatalogoDbContext> options) : base(options) { }
+
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+}
